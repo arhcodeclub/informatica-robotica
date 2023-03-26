fa5d6989febf752ab47e71b96063318696095e5c
MrIsaak14 <101879866+MrIsaak14@users.noreply.github.com>
2023-03-26 23:30:44 +0200

    Update script.js

--- src/webinterface/views/script.js
+++ src/webinterface/views/script.js
@@ -3,51 +3,105 @@ const buttonLeft = document.getElementById("buttonLeft")
 const buttonRight = document.getElementById("buttonRight")
 const buttonDown = document.getElementById("buttonDown")
 
-buttonUp.addEventListener("click", function() {
-    up()
+
+
+buttonUp.addEventListener("mousedown", function() {
+    upStart()
+})
+buttonUp.addEventListener("mouseup", function() {
+    upStop()
 })
 window.addEventListener("keydown", function(e) {
     if (e.key === "w" || e.keyCode === 38) {
-        up()
+        upStart()
+    }
+})
+window.addEventListener("keyup", function(e) {
+    if (e.key === "w" || e.keyCode === 38) {
+        upStop()
     }
 })
 
-buttonLeft.addEventListener("click", function() {
-    left()
+
+
+buttonLeft.addEventListener("mousedown", function() {
+    leftStart()
+})
+buttonLeft.addEventListener("mouseup", function() {
+    leftStop()
 })
 window.addEventListener("keydown", function(e) {
     if (e.key === "a" || e.keyCode === 37) {
-        left()
+        leftStart()
+    }
+})
+window.addEventListener("keyup", function(e) {
+    if (e.key === "a" || e.keyCode === 37) {
+        leftStop()
     }
 })
 
-buttonRight.addEventListener("click", function() {
-    right()
+
+
+buttonRight.addEventListener("mousedown", function() {
+    rightStart()
+})
+buttonRight.addEventListener("mouseup", function() {
+    rightStop()
 })
 window.addEventListener("keydown", function(e) {
     if (e.key === "d" || e.keyCode === 39) {
-        right()
+        rightStart()
+    }
+})
+window.addEventListener("keyup", function(e) {
+    if (e.key === "d" || e.keyCode === 39) {
+        rightStop()
     }
 })
 
-buttonDown.addEventListener("click", function() {
-    down()
+
+
+buttonDown.addEventListener("mousedown", function() {
+    downStart()
+})
+buttonDown.addEventListener("mouseup", function() {
+    downStop()
 })
 window.addEventListener("keydown", function(e) {
     if (e.key === "s" || e.keyCode === 40) {
-        down()
+        downStart()
+    }
+})
+window.addEventListener("keyup", function(e) {
+    if (e.key === "s" || e.keyCode === 40) {
+        downStop()
     }
 })
 
-function up() {
-    console.log("up")
+
+
+function upStart() {
+    console.log("up: start")
 }
-function left() {
-    console.log("left")
+function upStop() {
+    console.log("up: stop")
 }
-function right() {
-    console.log("right")
+function leftStart() {
+    console.log("left: start")
 }
-function down() {
-    console.log("down")
+function leftStop() {
+    console.log("left: stop")
+}
+function rightStart() {
+    console.log("right: start")
+}
+function rightStop() {
+    console.log("right: stop")
+}
+function downStart() {
+    console.log("down: start")
+}
+function downStop() {
+    console.log("down: stop")
 }
