89a411588262806e7f4b67fb4637f04b381dd8a9
Coenicorn <coenicorn.blauwzee@gmail.com>
2023-03-31 17:50:36 +0200

    removed comment

--- src/webinterface/app.js
+++ src/webinterface/app.js
@@ -11,39 +11,6 @@ app.post("/action", (req, res)=>{
     console.log(req.body); 
 });
 
-// app.post('/upstart', (req, res) => {
-//     console.log("up: start")
-//     res.end("hello world")                                             
-// })
-// app.post('/leftstart', (req, res) => {
-//     console.log("left: start")
-//     res.end("hello world")
-// })
-// app.post('/rightstart', (req, res) => {
-//     console.log("right: start")
-//     res.end("hello world")
-// })
-// app.post('/downstart', (req, res) => {
-//     console.log("down: start")
-//     res.end("hello world")
-// })
-// app.post('/upstop', (req, res) => {
-//     console.log("up: stop")
-//     res.end("hello world")
-// })
-// app.post('/leftstop', (req, res) => {
-//     console.log("left: stop")
-//     res.end("hello world")
-// })
-// app.post('/rightstop', (req, res) => {
-//     console.log("right: stop")
-//     res.end("hello world")
-// })
-// app.post('/downstop', (req, res) => {
-//     console.log("down: stop")
-//     res.end("hello world")
-// })
-
 app.listen(3000, ()=>{
     console.log("Started!");
 });
