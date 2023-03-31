5bf5e811bef4f9150f77bbe674e31f9646134477
Coenicorn <coenicorn.blauwzee@gmail.com>
2023-03-31 17:50:03 +0200

    Fixed backend and frontend

--- src/webinterface/app.js
+++ src/webinterface/app.js
@@ -1,46 +1,49 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require("express");
+const parser = require("body-parser");
 
 const app = express()
 
-app.listen(3000)
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(morgan('dev'))
+app.use(express.static("public"));
+app.use(parser.urlencoded({ extended: true }))
+app.use(parser.json());
 
-app.post('/upstart', (req, res) => {
-    console.log("up: start")
-    res.end("hello world")                                             
-})
-app.post('/leftstart', (req, res) => {
-    console.log("left: start")
-    res.end("hello world")
-})
-app.post('/rightstart', (req, res) => {
-    console.log("right: start")
-    res.end("hello world")
-})
-app.post('/downstart', (req, res) => {
-    console.log("down: start")
-    res.end("hello world")
-})
-app.post('/upstop', (req, res) => {
-    console.log("up: stop")
-    res.end("hello world")
-})
-app.post('/leftstop', (req, res) => {
-    console.log("left: stop")
-    res.end("hello world")
-})
-app.post('/rightstop', (req, res) => {
-    console.log("right: stop")
-    res.end("hello world")
-})
-app.post('/downstop', (req, res) => {
-    console.log("down: stop")
-    res.end("hello world")
-})
+app.post("/action", (req, res)=>{
+    console.log(req.body); 
+});
 
-app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+// app.post('/upstart', (req, res) => {
+//     console.log("up: start")
+//     res.end("hello world")                                             
+// })
+// app.post('/leftstart', (req, res) => {
+//     console.log("left: start")
+//     res.end("hello world")
+// })
+// app.post('/rightstart', (req, res) => {
+//     console.log("right: start")
+//     res.end("hello world")
+// })
+// app.post('/downstart', (req, res) => {
+//     console.log("down: start")
+//     res.end("hello world")
+// })
+// app.post('/upstop', (req, res) => {
+//     console.log("up: stop")
+//     res.end("hello world")
+// })
+// app.post('/leftstop', (req, res) => {
+//     console.log("left: stop")
+//     res.end("hello world")
+// })
+// app.post('/rightstop', (req, res) => {
+//     console.log("right: stop")
+//     res.end("hello world")
+// })
+// app.post('/downstop', (req, res) => {
+//     console.log("down: stop")
+//     res.end("hello world")
+// })
+
+app.listen(3000, ()=>{
+    console.log("Started!");
 });
