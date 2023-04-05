99a55fee08568cb9ad26da6a5c20f4a75385f737
coenicorn.blauwzee@gmail.com <coenicorn.blauwzee@gmail.com>
2023-04-05 15:47:38 +0200

    fixed npm packages

--- webinterface/app.js
+++ webinterface/app.js
@@ -4,7 +4,7 @@ const _ = require("lodash");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("./public"));
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 
