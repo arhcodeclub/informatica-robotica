e845c2c4c1dabb65c00cc49778a0ed5220f977ba
coenicorn.blauwzee@gmail.com <coenicorn.blauwzee@gmail.com>
2023-04-07 21:06:59 +0200

    completely overengineered input handling

--- webinterface/public/script.js
+++ webinterface/public/script.js
@@ -1,19 +1,144 @@
-const buttonUp = document.querySelector("#buttonUp");
-const buttonLeft = document.getElementById("buttonLeft");
-const buttonRight = document.getElementById("buttonRight");
-const buttonDown = document.getElementById("buttonDown");
-const buttons = [buttonUp, buttonLeft, buttonRight, buttonDown];
-const directions = ["up", "left", "right", "down"];
-const keys = ["w", "a", "d", "s"];
-const arrows = [38, 37, 39, 40];
+class KeyboardInputContainer {
 
-let stopCount = 0;
-let stopSave = 1;
+    key;
+    isPressed;
 
-/** sets button style for pressed/released */
-function setButtonPressedCss(button, isPressed) {
-    if (isPressed) button.style.border = "rgba(255, 255, 255, 0.2) solid 2px";
-    else button.style.border = "rgba(255, 255, 255, 1) solid 2px";
+    /**
+     * @param key The key with which this button activates
+     */
+    constructor(key) {
+        this.key = key;
+
+        this.isPressed = false;
+    }
+
+    press() {
+        this.isPressed = true;
+    }
+
+    unpress() {
+        this.isPressed = false;
+    }
+
+}
+
+class CarInput {
+
+    keyboardInputs;
+    element;
+    activateCallback;
+    deactivateCallback;
+
+    isPressed;
+    isClicked;
+
+    /**
+     * @param element HTMLElement to bind to
+     * @param keys Array of keys that should activate this button
+     * @param activateCallback Callback function called on activation
+     * @param deactivateCallback Callback function called on deactivation
+     */
+    constructor(element, keys, activateCallback, deactivateCallback) {
+        this.element = element;
+        this.activateCallback = activateCallback;
+        this.deactivateCallback = deactivateCallback;
+
+        this.keyboardInputs = [];
+
+        keys.forEach(element => {
+            
+            this.keyboardInputs.push(new KeyboardInputContainer(element));
+
+        });
+
+        this.isClicked = false;
+    
+        this.initialize();
+    }
+
+    setStyle(isActive) {
+        if (isActive) this.element.style.border = "rgba(255, 255, 255, 0.2) solid 2px";
+        else this.element.style.border = "rgba(255, 255, 255, 1) solid 2px";
+    }
+
+    activate() {
+        if (this.isPressed || this.isClicked) return;
+
+        this.setStyle(1);
+
+        this.activateCallback();
+    }
+
+    deactivate() {
+        if (this.isPressed || this.isClicked) return;
+
+        this.setStyle(0);
+
+        this.deactivateCallback();
+    }
+
+    _click() {
+        this.activate();
+        
+        this.isClicked = true;
+    }
+
+    _unclick() {
+        this.isClicked = false;
+
+        this.deactivate();
+    }
+
+    _press(e) {
+        let key = e.key;
+
+        this.keyboardInputs.forEach(kinput => {
+            if (kinput.isPressed) return;
+           
+            if (key === kinput.key) {
+                kinput.press();
+
+                this.activate();
+
+                this.isPressed = true;
+
+                return;
+            }
+        });
+    }
+
+    _unpress(e) {
+        let key = e.key;
+
+        let pressed = 0, isMine = false;
+
+        this.keyboardInputs.forEach(kinput => {
+            if (key === kinput.key) {
+                kinput.unpress();
+
+                isMine = true;
+            }
+            else if (kinput.isPressed) pressed++;
+        });
+
+        if (isMine && !pressed) {
+            this.isPressed = false;
+
+            this.deactivate();
+        }
+    }
+
+    /* attaches listeners to this.element */
+    initialize() {
+
+        this.element.addEventListener("mousedown", this._click.bind(this));
+        this.element.addEventListener("mouseup", this._unclick.bind(this));
+
+        document.addEventListener("keydown", this._press.bind(this));
+        document.addEventListener("keyup", this._unpress.bind(this));
+
+    }
+
 }
 
 /** sends [data] to server */
@@ -43,50 +168,12 @@ function sendDirection(data) {
     sendDataToServer(data);
 }
 
-/** initializes singular button */
-function initButton(button, direction, arrow, key) {
-    button.addEventListener("mousedown", function () {
-        sendDirection({
-            direction,
-            start: 1,
-        });
-    });
-    button.addEventListener("mouseup", function () {
-        sendDirection({
-            direction,
-            start: 0,
-        });
-        setButtonPressedCss(button, 0);
-    });
-    window.addEventListener("keydown", function (e) {
-        if (e.key === key || e.keyCode === arrow) {
-            if (stopCount != stopSave) {
-                sendDirection({
-                    direction,
-                    start: 1,
-                });
-            }
-            setButtonPressedCss(button, 1);
-            stopSave = stopCount;
-        }
-    });
-    window.addEventListener("keyup", function (e) {
-        if (e.key === key || e.keyCode === arrow) {
-            sendDirection({
-                direction,
-                start: 0,
-            });
-            setButtonPressedCss(button, 0);
-            stopCount++;
-        }
-    });
-}
-
 /** program entrypoint (though javascript doesn't really have that :/) */
 function main() {
-    for (let i = 0; i < 4; i++) {
-        initButton(buttons[i], directions[i], arrows[i], keys[i]);
-    }
+    new CarInput(document.getElementById("buttonUp"), ["w", "ArrowUp"], ()=>{ sendDirection({ direction: "up", start: 1 }) }, ()=>{ sendDirection({ direction: "up", start: 0 }) })
+    new CarInput(document.getElementById("buttonDown"), ["s", "ArrowDown"], ()=>{ sendDirection({ direction: "down", start: 1 }) }, ()=>{ sendDirection({ direction: "down", start: 0 }) })
+    new CarInput(document.getElementById("buttonLeft"), ["a", "ArrowLeft"], ()=>{ sendDirection({ direction: "left", start: 1 }) }, ()=>{ sendDirection({ direction: "left", start: 0 }) })
+    new CarInput(document.getElementById("buttonRight"), ["d", "ArrowRight"], ()=>{ sendDirection({ direction: "right", start: 1 }) }, ()=>{ sendDirection({ direction: "right", start: 0 }) })
 }
 
 onload = main;
